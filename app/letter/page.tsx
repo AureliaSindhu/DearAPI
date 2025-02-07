55dbdd507aa8e452ff226d95ba755cdead0a401e
Aurelia Sindhu <100260518+AureliaSindhu@users.noreply.github.com>
2025-02-06 18:39:30 -0800

    form ui

--- app/letter/page.tsx
+++ app/letter/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ValentineForm from '@/components/ValentineForm';
+import Footer from '@/components/Footer';
 
 const LetterPage: React.FC = () => {
     return (
-        <div className="flex justify-center items-center h-screen bg-gray-100">
+        <div className="flex justify-center items-center h-screen bg-rose-950">
             <ValentineForm />
+            <Footer />
         </div>
     );
 };
